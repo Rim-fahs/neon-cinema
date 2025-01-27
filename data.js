86fa43b89c9714eb816a8321f3d104d944f46b3d
Rim-fahs <rimfahs.business@gmail.com>
2025-01-27 23:54:17 +0200

    feat : create whatson page with its style sheet

---
+++ data.js
@@ -0,0 +1,11 @@
+export const data = [
+  {
+    id: 1,
+    title: "Interstellar",
+    rating: "PG",
+    poster: "movie posters/posters/movie 1.jpg",
+    trailer: "https://www.youtube.com/embed/zSWdZVtXT7E?si=N1-ioabJmkskKGGP",
+    description:
+      "With humanity teetering on the brink of extinction, a group of astronauts travels through a wormhole in search of another inhabitable planet.",
+  },
+];
---
+++ rating.js
@@ -0,0 +1,53 @@
+const wrapper = document.querySelector(".review");
+const stars = document.querySelectorAll(".fa-star");
+const button = document.querySelector("button");
+console.log(button.style);
+
+const clearActive = () => {
+  //remove active class from all stars
+  stars.forEach((star) => {
+    star.classList.remove("active");
+  });
+};
+
+const addActive = (star, index) => {
+  //add active class to current star
+  star.classList.add("active");
+  let count = index;
+
+  //add active class to previous star from current start
+  while (count > 0) {
+    document.getElementById(`star-${count}`).classList.add("active");
+    count--;
+  }
+};
+
+
+//handle star ratings
+stars.forEach((star, index) =>
+  star.addEventListener("click", () => {
+    clearActive();
+    addActive(star, index);
+  })
+);
+
+
+const modal = document.getElementById("thank-you-modal");
+const closeModal = document.getElementById("close-modal");
+const textarea = document.querySelector("textarea");
+
+const onSubmit = () => {
+  // Show the modal
+  modal.classList.remove("hidden");
+};
+
+closeModal.addEventListener("click", () => {
+  // Hide the modal
+  modal.classList.add("hidden");
+
+  // Reset the stars to their default state
+  clearActive();
+
+  // Clear the textarea value
+  textarea.value = "";
+});
