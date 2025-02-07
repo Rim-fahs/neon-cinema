c4ef367cbe6de901ab43a22a6155a036bee7e657
Rim-fahs <rimfahs.business@gmail.com>
2025-02-07 23:52:08 +0200

    fixes

--- showtime.js
+++ showtime.js
@@ -2,6 +2,9 @@
 function openPopup(event) {
     event.preventDefault(); 
     document.getElementById("thankYouPopup").style.display = "flex";
+
+    // Reset the form inputs
+    document.getElementById("booknow-form").reset();
 }
 
 function closePopup() {
